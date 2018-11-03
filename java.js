ad9b35f81e6f267a7ab6c7a0725b0a621266bf3b
AbramovYuri <43909379+AbramovYuri@users.noreply.github.com>
2018-11-04 00:38:12 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -11,19 +11,14 @@ let screenWidth = document.querySelector('.demo-screen').offsetWidth;
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let canvasWidth, canvasHeight;
-let mX = 0;
-let mY = 0;
 let pointCoord = [];
 let starsCount = 300;
 let deepStars = 3000;
 let starsSpeed = 10;
 let k = 3000;
-let pointDistance = 150;
-let pointDistance3D = 500;
 
 
 addEventListener('resize', resize);
@@ -45,8 +40,10 @@ window.onload = mainLoop();
 document.querySelector('.demo-screen').addEventListener('click', mainLoop);
 
 function mainLoop() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    pointSet();
     flyingScroller();
-    mainLoop2();
+    pointMove();
     requestAnimationFrame(mainLoop);
 }
 
@@ -61,12 +58,12 @@ function flyingScroller() {
             sin = Math.sin(sinusCount);
             cos = Math.cos(cosCount);
             letter[i].style.left = Math.floor(sin * (screenHeight / 10)) + i * 30 - scrollLeft + "px";
-            letter[i].style.top = Math.floor(cos*80+screenHeight / 2) + Math.floor(sin * (screenHeight / 10)) + "px";
-            // letter[i].style.fontSize = Math.floor(40+sin*sin * 80) + "px";
+            letter[i].style.top = Math.floor(cos*80+screenHeight / 2) + Math.floor((sin*cos) * (screenHeight /10)) + "px";
+            // letter[i].style.fontSize = Math.floor(40+sin*sin * 40) + "px";
             letter[i].style.display = "inline-block";
             test++;
         }
-        sinusCount += .2;
+        sinusCount += .1;
 
     }
 
@@ -75,14 +72,13 @@ function flyingScroller() {
     } else {
         sinusCount = 0;
     }
-    cosCount +=.05;
+    cosCount +=.02;
     if (scrollLeft / 30 === scrollDelay) {
         scrollDelay++;
         letter[curentLetter].style.display = "none";
         curentLetter++;
     }
 
-    console.log(test);
 }
 
 function generateHtmlLetterScroller() {
@@ -98,23 +94,6 @@ function generateHtmlLetterScroller() {
 
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-/*
-document.querySelector('.universe').onmousemove = function (event) {
-        mX = (canvasWidth / 2 - event.clientX)/2;
-        mY = (canvasHeight / 2 - event.clientY)/2;
-};*/
-
-
-
-function mainLoop2() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    pointSet();
-   // lineDraw();
-    pointMove();
-
-}
-
 
 function pointGenerator() {
     let x, y, z, s;
@@ -169,51 +148,4 @@ function resize() {
     starsCount = canvasWidth*canvasHeight/6000;            //  количество звезд автоматически
     console.log(starsCount);
     pointGenerator();
-}
-
-function lineDraw() {
-    let vector, xCord, yCord, zCord;
-
-    for (let i = 0; i < starsCount - 1; i++) {
-
-        let x = pointCoord[i][0];
-        let y = pointCoord[i][1];
-        let z = pointCoord[i][2];
-
-        for (let q = 1; q < starsCount/6; q++) {
-
-            xCord = Math.abs(x - pointCoord[q][0]);
-            yCord = Math.abs(y - pointCoord[q][1]);
-            zCord = Math.abs(z - pointCoord[q][2]);
-
-            vector = Math.sqrt((xCord * xCord) + (yCord * yCord) + (zCord * zCord));
-
-            if (vector > 5 && vector < pointDistance3D) {
-                let p = pointCoord[i][2];
-                let x = (k * pointCoord[i][0]) / (p + k) + canvasWidth / 2 + mX;
-                let y = (k * pointCoord[i][1]) / (p + k) + canvasHeight / 2 + mY;
-
-                let p2 = pointCoord[q][2];
-                let x2 = (k * pointCoord[q][0]) / (p2 + k) + canvasWidth / 2 + mX;
-                let y2 = (k * pointCoord[q][1]) / (p2 + k) + canvasHeight / 2 + mY;
-
-                xCord = Math.abs(x - x2);
-                yCord = Math.abs(y - y2);
-
-                vector = Math.sqrt((xCord * xCord) + (yCord * yCord));
-
-                if (vector > 5 && vector < pointDistance) {
-
-                    ctx.beginPath();
-                    ctx.strokeStyle = 'rgba(200,200,200,' + ((1 - vector / pointDistance) / 4) + ')';
-                    ctx.moveTo(x, y);
-                    ctx.lineTo(x2, y2);
-                    ctx.stroke();
-                }
-            }
-
-
-        }
-    }
-
 }
